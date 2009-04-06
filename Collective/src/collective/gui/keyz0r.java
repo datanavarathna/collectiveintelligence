08250feb4d9206f57a5c2d5b7a805359b32047e2
PaulTag <PaulTag@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-06 01:00:38 +0000

    Placed code where it belongs. Put that bitch in its place

---
+++ Collective/src/collective/gui/keyz0r.java
@@ -0,0 +1,19 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class keyz0r implements KeyListener {
+
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+}
