445d3ccf6604a7ac7d9f51568090c4987cd9f0f0
CelenAzrael <CelenAzrael@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-18 23:04:24 +0000

    QuadTree stuff added.

---
+++ Collective/src/collective/agents/QuadTree.java
@@ -0,0 +1,64 @@
+public class QuadTree{
+  QuadNode head = null;
+  int length = 0;
+  public QuadTree(){
+    
+  }
+  
+  public QuadTree(Object o){
+    head = new QuadNode(o);
+    length = 1;
+  }
+  
+  public QuadTree(Object[] os){
+    for(int i = os.length-1; i >= 0; i++){
+      QuadNode added = new QuadNode(os[i]);
+      added.next = head;
+    }
+    length = os.length;
+  }
+  
+  public void addAtBeg(Object o){
+    QuadNode added = new QuadNode(o);
+    added.next = head;
+    length++;
+  }
+  
+  public Object find(Object o){
+    QuadNode ln = head;
+    while(ln.next!=null){
+    	ln = ln.next;
+    	if (ln.obj.equals(o))
+        	return o;
+    }
+    if(ln.obj.equals(o))
+    	return o;
+    return null;
+  }
+  
+  public Object[] returnAll(){
+	  Object[] op = new Object[length];
+	  QuadNode ln = head;
+	  for(int i = 0; i < length; i++){
+		  op[i]=ln;
+		  ln = ln.next;
+	  }
+	  return op;
+  }
+  
+  public int remove(Object o){
+	  QuadNode ln = head;
+	  if(ln.obj.equals(o)){
+		  head = ln.next;
+		  ln.next = null;
+		  return 1;
+	  }
+	  while(ln.next!=null){
+	    if(ln.next.obj.equals(o)){
+	    	ln.next = ln.next.next;
+	    	return 1;
+	    }
+	    ln = ln.next;	
+	  }
+  }
+}
