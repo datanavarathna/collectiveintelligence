a6e13108ab0f555a37f5a1cdf0cc5e30614631b6
DataNavarathna <DataNavarathna@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-18 19:40:17 +0000

    Created Scala wrapper for gui.

--- Collective/src/collective/agents/QuadTree.scala
+++ Collective/src/collective/agents/QuadTree.scala
@@ -3,8 +3,24 @@ package collective.agents
 import agents.DataClasses
 
 class QuadTree {
+  var obstacles = Nil //new List[Obstacle]
+  
   def contains(x: Int,y: Int):Boolean
-  def add(x: Int,y: Int):Boolean //returns true if successful
-  def remove(x: Int,y: Int):Boolean //returns true if successful
-  def range(minX: Int, minY: Int, maxX: Int, maxY: Int):List[Obstacle]
+ 
+	def add(x: Int,y: Int):Boolean //returns true if successful
+ 
+	def remove(x: Int,y: Int):Boolean //returns true if successful
+ 
+	def range(sensorRange: Int, maxX: Int, maxY: Int):List[Obstacle] = 
+	{
+			for (
+					obstacle: Obstacle <- obstacles
+					if(obstacle.x*obstacle.x* + obstacle.y*obstacle.y <= sensorRange)
+			) yield {}
+			
+	}
+
+	
+
+
 }
---
+++ Collective/src/collective/gui/scalaGui.scala
@@ -0,0 +1,7 @@
+package collective.gui
+
+object scalaGui {
+	def main(args: Array[String]) {
+		//instance of gui class
+	}
+}
