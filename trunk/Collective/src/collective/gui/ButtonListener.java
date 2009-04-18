c397f416809dede9cf37672f20d5a542a993bcc9
PaulTag <PaulTag@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-18 20:33:59 +0000

    Added more to the GUI class. Updated to work with Ctrl Pane, and working with agent processing for inits.

--- trunk/Collective/src/collective/gui/ButtonListener.java
+++ trunk/Collective/src/collective/gui/ButtonListener.java
@@ -7,16 +7,29 @@ public class ButtonListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		if ( e.getModifiers() == 16 ) {
+			clicked(e);
+		} else if ( e.getModifiers() == 18 ) {
+			ctrlClicked(e);
+		}
+		
+	}
+	
+	private void ctrlClicked( ActionEvent e ) {
+		GUIObject go = (GUIObject) e.getSource();
+		System.out.println(go.getStatus());
+	}
+	
+	private void clicked( ActionEvent e ) {
 		GUIObject go = (GUIObject) e.getSource();
 		
 		int status = go.getStatus();
 		int sStatus = 0;
 		
 		if ( status == 0 ) {
-			sStatus = 2;
+			sStatus = 1;
 		} else if ( status == 1 ) {
-			System.err.println("Agent is On this Block! Unable to Change!");
-			sStatus = status;
+			sStatus = 2;
 		} else if ( status == 2 ) {
 			sStatus = 0;
 		} else {
@@ -24,7 +37,6 @@ public class ButtonListener implements ActionListener {
 		}
 		
 		go.setStatus( sStatus );
-		
-		
 	}
+	
 }
