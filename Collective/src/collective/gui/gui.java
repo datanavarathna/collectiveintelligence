36bcda9fb13b327a21d3ae4420a73529548c3e8e
PaulTag <PaulTag@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-18 20:33:59 +0000

    Added more to the GUI class. Updated to work with Ctrl Pane, and working with agent processing for inits.

--- Collective/src/collective/gui/gui.java
+++ Collective/src/collective/gui/gui.java
@@ -2,15 +2,31 @@ package collective.gui;
 
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class gui {
 
 	int x, y;
 	JFrame frame;
+	JFrame ctrl;
+	
 	ArrayList <GUIObject> objects;
-
+	
+	ControlButtonListener cl;
+	ButtonListener bl;
+	
+	public gui() {
+		this.setup( 5, 5, 500, 500 );
+	}	
 	public gui( int x, int y ) {
+		this.setup( x, y, 500, 500 );
+	}
+	public gui( int x, int y, int windowX, int windowY ) {
+		this.setup( x, y, windowX, windowY );
+	}
+
+	private void setup( int x, int y, int windowX, int windowY ) {
 		
 		this.objects = new ArrayList<GUIObject>();
 		
@@ -19,30 +35,57 @@ public class gui {
 
 		this.frame = new JFrame("Collective Status Pane");
 		this.frame.setLayout( new GridLayout ( this.x, this.y ) ) ;
+
+		this.ctrl = new JFrame("Controller");
+		this.ctrl.setLayout( new GridLayout ( 1, 5 ) ) ;
 		
-		ButtonListener bl = new ButtonListener();
+		this.bl = new ButtonListener();
+		this.cl = new ControlButtonListener();
 		
-		this.fill( bl );
+		this.fill();
 		
 		this.frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		this.frame.setSize(600, 600);
+		this.ctrl.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		this.frame.setSize(windowX, windowY);
+		this.ctrl.setSize(300, 75);
+		
 		frame.setVisible(true);
+		ctrl.setVisible(true);
+		
 	}
-
-	private void fill( ButtonListener bl ) {
+	
+	private void fill() {
 		
 		int count = this.x * this.y;
 
 		for ( int i = 0; i < count; ++i ) {
 			GUIObject o = new GUIObject( 0 );
 			
-			o.addActionListener(bl);
+			o.addActionListener( this.bl );
 			
 			this.objects.add(o);
 			o.setStatus( 0 );
 			
 			frame.getContentPane().add(o);
 		}
+		
+		JButton jb = new JButton();
+		jb.setText("initialize");
+		jb.addActionListener(this.cl);
+		this.ctrl.getContentPane().add(jb);
+		
+		
+		jb = new JButton();
+		jb.setText("start");
+		jb.addActionListener(this.cl);
+		this.ctrl.getContentPane().add(jb);
+		
+		jb = new JButton();
+		jb.setText("kill");
+		jb.addActionListener(this.cl);
+		this.ctrl.getContentPane().add(jb);
+		
 	}
 
 	public synchronized GUIObject getByXY( int x, int y ) {
@@ -53,6 +96,10 @@ public class gui {
 			o = this.objects.get(index);
 		}
 		return o;
+	}
+	
+	public void setGUISize( int x, int y ) {
+		this.frame.setSize(x, y);
 	}
 	
 	public synchronized void updateCellAgentStatus( int x, int y, boolean agentInSquare ) {
@@ -68,13 +115,16 @@ public class gui {
 			System.err.println("Invalid Index!");
 		}
 	}
+	
 	public void refresh() {
 		this.frame.repaint();
 	}
 
 	public static void main ( String[] args ) {
 
-		gui g = new gui( 5, 5 );
+		gui g = new gui( 5, 5, 100, 100 );
+		
+		g.setGUISize(600, 600);
 		
 		g.updateCellAgentStatus(0, 0, true);
 		g.updateCellAgentStatus(1, 1, true);
