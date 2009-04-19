106e19566aa655910716635d5afa08fa84042c8e
DataNavarathna <DataNavarathna@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-19 00:42:30 +0000

    In the middle of translating QuadTreeGateway into true Scala. File is currently broken.

--- Collective/src/collective/agents/ObstacleJava.java
+++ Collective/src/collective/agents/ObstacleJava.java
@@ -4,4 +4,20 @@ public class ObstacleJava {
 	int x;
 	int y;
 	int Otype;
+	
+	
+	
+	public ObstacleJava(int otype, int x, int y) {
+		super();
+		Otype = otype;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public ObstacleJava(){
+		this.x = 0;
+		this.y = 0;
+		this.Otype = 0;
+	}
+	
 }
