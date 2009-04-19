06fb5566a17fccbaa6741902b00dda1d667db6c7
DataNavarathna <DataNavarathna@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-19 00:42:30 +0000

    In the middle of translating QuadTreeGateway into true Scala. File is currently broken.

--- trunk/Collective/src/collective/agents/QuadTreeGateway.scala
+++ trunk/Collective/src/collective/agents/QuadTreeGateway.scala
@@ -1,70 +1,87 @@
 package collective.agents
+
 import collective.agents.CaseClasses
 import collective.agents.QuadTree
-class QuadTreeGateway {
-  private val qT = QuadTree
-  def add(o: Obstacle): Boolean =
-    o2 = ObstacleJava;
-    o2.x = o.x;
-    o2.y = o.y;
-    o2.Otype = o.Obstacletype;
-    qT.add(o2);
-    true
-  
-   def add(type: Int; x: Int; y: Int): Boolean =
-    o2 = ObstacleJava;
-    o2.Otype = type;
-    o2.x = x;
-    o2.y = y;
-    qT.add(o2);
-    true
-  
-   def remove(o: Obstacle): Boolean =
-     o2 = ObstacleJava;
-     o2.x = o.x;
-     o2.y = o.y;
-     o2.Otype = o.ObstacleType;
-     outp = qT.remove(o2);
-     outp
-   
-   def remove(type: Int; x: Int; y: Int): Boolean =
-    o2 = ObstacleJava;
-    o2.Otype = type;
-    o2.x = x;
-    o2.y = y;
-    o2.Otype = o.ObstacleType;
-    outp = qT.remove(o2);
-    outp
-    
-   def contains(o: Obstacle): Boolean =
-     o2 = ObstacleJava;
-     o2.x = o.x;
-     o2.y = o.y;
-     o2.Otype = o.ObstacleType;
-     outp = qT.find(o2);
-     outp
-     
-   def contains(type: Int; x: Int; y: Int): Boolean =
-     o2 = ObstacleJava;
-     o2.Otype = type;
-     o2.x = x;
-     o2.y = y;
-     o2.Otype = o.ObstacleType;
-     outp = qT.remove(o2);
-     outp
-    
-   def round(radius: Int; x: Int; y: Int): List(Of Obstacle) =
-     input = qT.returnAll();
-     output = List(Of Obstacle);
-     for(i = 0; i < input.length; i++){
-       obst = (ObstacleJava)input[i];
-       if(((x-obst.x)^2+(y-obst.y)^2)^(0.5)<=radius){
-         obst2 = Obstacle;
-         obst2.x = obst.x;
-         obst2.y = obst.y;
-         obst2.ObstacleType = obst.OType;
-         output.add(obst2)
-       }
-     }
-     output
+
+class QuadTreeGateway 
+{
+	private val qT = new QuadTree()
+	def add(o: Obstacle): Boolean =
+	{
+			o2 = new ObstacleJava();
+			o2.x = o.x;
+			o2.y = o.y;
+			o2.Otype = o.Obstacletype;
+			qT.add(o2)
+			true
+	}
+	
+	def add(obstacleType: Int, x: Int, y: Int): Boolean =
+	{
+			o2 = new ObstacleJava()
+			o2.Otype = obstacleType
+			o2.x = x
+			o2.y = y
+			qT.add(o2)
+			true
+	}
+
+	def remove(o: Obstacle): Boolean =
+	{
+			o2 = new ObstacleJava()
+			o2.x = o.x
+			o2.y = o.y
+			o2.Otype = o.ObstacleType
+			outp = qT.remove(o2)
+			outp
+	}
+
+	def remove(obstacleType: Int, x: Int, y: Int): Boolean =
+	{
+			o2 = new ObstacleJava()
+			o2.Otype = obstacleType
+			o2.x = x
+			o2.y = y
+			o2.Otype = o.ObstacleType
+			outp = qT.remove(o2)
+			outp
+	}
+
+	def contains(o: Obstacle): Boolean =
+	{
+			o2 = new ObstacleJava()
+			o2.x = o.x
+			o2.y = o.y
+			o2.Otype = o.ObstacleType
+			outp = qT.find(o2)
+			outp
+	}
+
+	def contains(obstacleType: Int, x: Int, y: Int): Boolean =
+	{
+			o2 = new ObstacleJava()
+			o2.Otype = obstacleType
+			o2.x = x
+			o2.y = y
+			o2.Otype = o.ObstacleType
+			outp = qT.remove(o2)
+			outp
+	}
+
+	def round(radius: Int, x: Int, y: Int): List[Obstacle] =
+	{
+		input = qT.returnAll()
+		output = List(Of Obstacle)
+		for(i = 0; i < input.length; i++){
+			obst = (ObstacleJava)input[i];
+			if(((x-obst.x)^2+(y-obst.y)^2)^(0.5)<=radius){
+				obst2 = Obstacle;
+				obst2.x = obst.x;
+				obst2.y = obst.y;
+				obst2.ObstacleType = obst.OType;
+				output.add(obst2)
+			}
+		}
+		output
+	}
 }
