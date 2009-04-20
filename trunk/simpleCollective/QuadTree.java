38b58e18b7272b21113c32935566cecfbc05ccd9
CelenAzrael <CelenAzrael@14b189e2-10e7-11de-99c7-9d618077940f>
2009-04-20 00:18:06 +0000

    |*|-|1><3|) |33/\/'5 *||_|4|) 7|233 |*|20|3|_3/\/\5.

--- trunk/simpleCollective/QuadTree.java
+++ trunk/simpleCollective/QuadTree.java
@@ -4,14 +4,14 @@ public class QuadTree{
   QuadNode root = null;
   int length = 0;
   public QuadTree(){
-    
+
   }
-  
+
   public QuadTree(Object o){
     root = new QuadNode(o);
     length = 1;
   }
-  
+
   public QuadTree(Object[] os){
     for(int i = os.length-1; i >= 0; i++){
       QuadNode added = new QuadNode(os[i]);
@@ -19,17 +19,17 @@ public class QuadTree{
     }
     length = os.length;
   }
-  
+
   public void add(Object o){
 	  addAtBeg(o);
   }
-  
+
   public void addAtBeg(Object o){
     QuadNode added = new QuadNode(o);
     added.next = root;
     length++;
   }
-  
+
   public Object find(Object o){
     QuadNode ln = root;
     if(root!=null){
@@ -43,22 +43,30 @@ public class QuadTree{
    }
    return null;
   }
-  
-  public boolean contains(Object o)
-  {
-	  return find(o) != null;
-  }
-  
-  public Object[] returnAll(){
-	  Object[] op = new Object[length];
-	  QuadNode ln = root;
-	  for(int i = 0; i < length; i++){
-		  op[i]=ln;
-		  ln = ln.next;
-	  }
-	  return op;
-  }
-  
+
+public boolean contains(Object o)
+{
+	return find(o) != null;
+}
+
+public Object[] returnAll(){
+	ArrayList<Object> al = new ArrayList();
+	if(root!=null){
+		QuadNode ln = root;
+		if(root.obj!=null)
+			al.add(root.obj);
+		while(ln.next !=null){
+			if(root.obj!=null)
+				al.add(ln.next.obj);
+		}
+		Object[] op = new Object[al.size()];
+		for(int i = 0; i < al.size(); i++){
+			op[i]=al.get(i);
+		}
+  	}
+	return op;
+}
+
   public Boolean remove(Object o){
 	  QuadNode ln = root;
 	  if(ln.obj.toString().equals(o.toString())){
@@ -71,7 +79,7 @@ public class QuadTree{
 	    	ln.next = ln.next.next;
 	    	return true;
 	    }
-	    ln = ln.next;	
+	    ln = ln.next;
 	  }
 	  return false;
   }
