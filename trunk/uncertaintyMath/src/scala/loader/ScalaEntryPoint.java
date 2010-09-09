656eb05ebf1c352c3c2dcc982eb7078d4a12edb1
DataNavarathna <DataNavarathna@14b189e2-10e7-11de-99c7-9d618077940f>
2010-09-09 18:28:00 +0000

    Added a check to the Measurement primary constructor to ensure that all Measurement values have uncertainties >= Double.Epsilon.

---
+++ trunk/uncertaintyMath/src/scala/loader/ScalaEntryPoint.java
@@ -0,0 +1,14 @@
+package scala.loader;
+
+public class ScalaEntryPoint
+{
+	public static void main(String[] args) {
+		/*
+		List<String> argList = new ArrayList<String>();
+		argList.add("UncertaintyMathTest");
+		for (String s : args) argList.add(s);
+		scala.tools.nsc.MainGenericRunner.main(argList.toArray(new String[0]));
+		*/
+		//sourceHandling.Compiler.main(args);
+	}
+}
